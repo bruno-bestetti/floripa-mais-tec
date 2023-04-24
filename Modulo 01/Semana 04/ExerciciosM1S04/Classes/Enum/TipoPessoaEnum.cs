358a3af9c992dd18f8ff81a7aafa626275d8904e
bruno-bestetti <bestettibruno@gmail.com>
2023-04-23 23:16:15 -0300

    Envio dos exercícios da quarta semana do Módulo 1
    Envio dos exercícios da quarta semana do Módulo 1

---
+++ Modulo 01/Semana 04/ExerciciosM1S04/Classes/Enum/TipoPessoaEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosM1S04.Classes.Enum
+{
+    public enum TipoPessoaEnum
+    {
+        FISICA,
+        JURIDICA
+    }
+}
---
+++ Modulo 01/Semana 04/ExerciciosM1S04/Program.cs
@@ -0,0 +1,26 @@
+﻿using ExerciciosM1S04.Classes.Enum;
+using ExerciciosM1S04.Classes;
+
+Cliente pessoaJuridica = new Cliente("Bruno Bestetti", DateTime.Parse("1991-11-02"), "Dev FullStack", "Solteiro", TipoPessoaEnum.JURIDICA);
+
+ContaEmpresarial contaJuridica = new ContaEmpresarial(1542, 1234, pessoaJuridica, 1500, 10, "04528241000123");
+
+contaJuridica.FazerEmprestimo(1200);
+contaJuridica.Depositar(200);
+contaJuridica.ExibirSaldo();
+contaJuridica.PagarEmprestimo();
+contaJuridica.ExibirSaldo();
+
+Cliente pessoaFisica = new Cliente("Bruno Costa", DateTime.Parse("1999-01-25"), "Desenvolvedor", "Casado", TipoPessoaEnum.FISICA);
+
+ContaCorrente contaCorrente = new ContaCorrente(1542, 1234, pessoaFisica);
+
+contaCorrente.Depositar(200);
+contaCorrente.Sacar(50);
+contaCorrente.ExibirSaldo();
+
+ContaPoupanca contaPoupanca = new ContaPoupanca(1542, 1234, pessoaFisica);
+
+contaPoupanca.Depositar(200);
+contaPoupanca.Transferir(contaCorrente, 50);
+contaPoupanca.ExibirSaldo();
